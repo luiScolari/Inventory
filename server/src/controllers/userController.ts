0635ff8ca414a1f66a6249d2b9c2b6fdd11ccafe
DESKTOP-DVA5H5S\Luis <luiscolari889@hotmail.com>
2025-10-17 21:36:14 -0300

    Teste commit

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 
 const prisma = new PrismaClient();
 
-export const getUsers = async (req: Request, res: Response): Promise<void> => {
+export const getUsers = async (req: Request, res: Response): Promise<void> => { // teste
   try {
     const users = await prisma.users.findMany();
     res.json(users);
